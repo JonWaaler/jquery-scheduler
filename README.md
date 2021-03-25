3d0538b60b2fb88b1a636e323e7d96df2a66d66e
Jon W <jonwaaler@outlook.com>
2021-03-25 00:52:51 -0400

    added save/load, fixed colouring, readme, css style changes

---
+++ README.md
@@ -0,0 +1,23 @@
+# Jon Waaler's jQuery Scheduler
+
+## Purpose
+
+To create a fully functional app using what I've learned in jQuery. I use [day.js](https://day.js.org/en/) api to get the time of day which then allows my to colour co-ordinate the scheduler. Grey = past, Red = current
+
+## Resources
+
+- [Day Suffix](https://stackoverflow.com/questions/13627308/add-st-nd-rd-and-th-ordinal-suffix-to-a-number)
+
+## Built With
+
+- HTML
+- CSS
+- JQuery/Javascript
+
+## Website
+
+https://jonwaaler.github.io/jquery-scheduler/
+
+## Contribution
+
+Made with ❤️ by Jon Waaler ✌
--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@
     </button>
 </div>
 */
+
 $(document).ready(function () {
   //-- Set up all 9 of the time blocks --//
   for (let i = 9; i < 13; i++) {
@@ -25,19 +26,17 @@ $(document).ready(function () {
   );
   $(".row").append('<button class="saveBtn"></button>');
   $(".saveBtn").append('<i class="far fa-save fa-2x"></i>');
-  //-- End of SetUP --//
 
+  //-- End of SetUP --//
   Set_TimeBlockColour();
+  Set_Date();
+  SaveTaskInfo();
+  GetTaskInfo();
 });
 
 // Every so often we want to check the time to see if we need to change
 // The time-block colour scheme around
-window.setInterval(Set_TimeBlockColour, 2000);
-
-// Also check the time at start
-function Set_CurrentHour(num) {
-  return num;
-}
+window.setInterval(Set_TimeBlockColour, 60000);
 
 //-- Change colour of the text areas based on time --//
 function Set_TimeBlockColour() {
@@ -62,34 +61,75 @@ function Set_TimeBlockColour() {
     // converts every time-block that's less than 6 to 24 hour time
     if (scheduleTime < 6) scheduleTime += 12;
 
-    // Decides the time block colour
+    // Decides the time-block colour
     if (scheduleTime == currentHour) {
-      $("div")
-        .eq(i + 1)
-        .children("textarea")
-        .removeClass("past present future");
-      $("div")
-        .eq(i + 1)
-        .children("textarea")
-        .addClass("present");
+      $(".row").eq(i).children("textarea").removeClass("past present future");
+      $(".row").eq(i).children("textarea").addClass("present");
     } else if (scheduleTime >= currentHour) {
-      $("div")
-        .eq(i + 1)
-        .children("textarea")
-        .removeClass("past present future");
-      $("div")
-        .eq(i + 1)
-        .children("textarea")
-        .addClass("future");
+      $(".row").eq(i).children("textarea").removeClass("past present future");
+      $(".row").eq(i).children("textarea").addClass("future");
     } else {
-      $("div")
-        .eq(i + 1)
-        .children("textarea")
-        .removeClass("past present future");
-      $("div")
-        .eq(i + 1)
-        .children("textarea")
-        .addClass("past");
+      $(".row").eq(i).children("textarea").removeClass("past present future");
+      $(".row").eq(i).children("textarea").addClass("past");
     }
   }
+}
+
+// taskValue is the info in the task that the user enters
+// timeKey is the key to set/get the task information
+// TODO: Save value to local Storage
+function SaveTaskInfo() {
+  // Save task
+  $(".saveBtn").click(function () {
+    // Organize key:value pairing
+    var taskValue = $(this).siblings("textarea").val();
+    var timeKey = $(this).siblings("h6").text();
+
+    // Save to localStorage
+    localStorage.setItem(timeKey, taskValue);
+  });
+}
+
+// TODO: get any task info from storage and put it on screen
+function GetTaskInfo() {
+  // For every time-block check if there's a stored key:value pair
+  for (let i = 0; i < 9; i++) {
+    // Get every h6 and check if it matches
+    var timeblockHour = $(".row").eq(i).children("h6").text();
+
+    var taskValue = localStorage.getItem(timeblockHour);
+    if (taskValue) {
+      $(".row").eq(i).children("textarea").val(taskValue);
+    }
+  }
+}
+
+function Set_Date() {
+  // Get the date and split information into an array
+  var formattedDate = dayjs().format("dddd-MMMM-D");
+  formattedDate = formattedDate.split("-");
+
+  // The suffix we give depends of what the number ends in
+  var daySuffix;
+  daySuffix = ordinal_suffix_of(parseInt(formattedDate[2]));
+
+  $("#currentDay").text(
+    `${formattedDate[0]}, ${formattedDate[1]} ${formattedDate[2]}${daySuffix}`
+  );
+}
+
+// Code from: https://stackoverflow.com/questions/13627308/add-st-nd-rd-and-th-ordinal-suffix-to-a-number
+function ordinal_suffix_of(i) {
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return "st";
+  }
+  if (j == 2 && k != 12) {
+    return "nd";
+  }
+  if (j == 3 && k != 13) {
+    return "rd";
+  }
+  return "th";
 }
